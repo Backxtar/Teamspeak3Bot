0975fc01dd1a52f7fdce0b6788feedc6f22a4429
Backxtar-sudo <58341469+Backxtar-sudo@users.noreply.github.com>
2021-10-14 20:14:08 +0200

    Added guildMission notifications

--- src/main/java/de/backxtar/systems/ChangeServerMessage.java
+++ src/main/java/de/backxtar/systems/ChangeServerMessage.java
@@ -7,37 +7,42 @@ import de.backxtar.Config;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ChangeServerMessage {
 
     public static void changeMessage(TS3Api api) {
-        String curMes = api.getServerInfo().getWelcomeMessage(), mes;
-        SimpleDateFormat sdf = null;
+        try {
+            String curMes = api.getServerInfo().getWelcomeMessage(), mes, date = "";
 
-        LocalDate now = Utils.localDate();
-        LocalDate nextEvent = Utils.getNextDate();
-        long value = Duration.between(now.atStartOfDay(), nextEvent.atStartOfDay()).toDays();
+            LocalDate now = Utils.localDate();
+            LocalDate nextEvent = Utils.getNextDate();
+            long value = Duration.between(now.atStartOfDay(), nextEvent.atStartOfDay()).toDays();
 
-        //Events
-        LocalDate halloween = LocalDate.of(now.getYear(), 10, 31);
-        LocalDate christmas = LocalDate.of(now.getYear(), 12, 24);
-        LocalDate newYear   = LocalDate.of(now.getYear(), 1, 1);
+            //Events
+            LocalDate halloween = LocalDate.of(now.getYear(), 10, 31);
+            LocalDate christmas = LocalDate.of(now.getYear(), 12, 24);
+            LocalDate newYear = LocalDate.of(now.getYear(), 1, 1);
 
-        if (Config.getConfigData().lang.equalsIgnoreCase("de"))
-            sdf = new SimpleDateFormat("dd.MM.yyyy");
-        if (Config.getConfigData().lang.equalsIgnoreCase("en"))
-            sdf = new SimpleDateFormat("MM-dd-yyyy");
+            if (Config.getConfigData().lang.equalsIgnoreCase("de"))
+                date = nextEvent.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            if (Config.getConfigData().lang.equalsIgnoreCase("en"))
+                date = nextEvent.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
 
-        if (sdf == null) return;
-        mes = Config.getConfigData().missionMessage.replaceAll("{date}", sdf.format(Utils.getNextDate()));
-        if (curMes.equalsIgnoreCase(mes)) return;
+            if (date.isEmpty()) return;
+            mes = Config.getConfigData().missionMessage.replaceAll("%date", date);
+            if (curMes.equalsIgnoreCase(mes)) return;
 
-        final Map<VirtualServerProperty, String> properties = new HashMap<>();
-        properties.put(VirtualServerProperty.VIRTUALSERVER_HOSTMESSAGE, mes);
-        if (value < 3) properties.put(VirtualServerProperty.VIRTUALSERVER_HOSTBANNER_MODE, "MODAL");
-        else properties.put(VirtualServerProperty.VIRTUALSERVER_HOSTBANNER_MODE, "NONE");
-        api.editServer(properties);
+            final Map<VirtualServerProperty, String> properties = new HashMap<>();
+            properties.put(VirtualServerProperty.VIRTUALSERVER_HOSTMESSAGE, mes);
+            if (value < 3) properties.put(VirtualServerProperty.VIRTUALSERVER_HOSTMESSAGE_MODE, "2");
+            else properties.put(VirtualServerProperty.VIRTUALSERVER_HOSTMESSAGE_MODE, "0");
+
+            api.editServer(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
